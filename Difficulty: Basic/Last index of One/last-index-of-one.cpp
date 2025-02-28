52d0f78d658160cb95f155c46b1e0184665fe363
Chandrakiran G <121334151+Kiran-VLSI@users.noreply.github.com>
2025-02-28 12:04:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Last index of One/last-index-of-one.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int lastIndex(string s) {
+        int i;
+        for(i=s.size()-1;i>=0;i--){
+            if(s[i]=='1'){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+//O(N)
+//space:O(1)
+
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.lastIndex(s) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
